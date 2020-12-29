b1c477b87f6c35f4f42b8da119e3522ee0c1eb00
fk96 <furkankale.1996@hotmail.com>
2020-12-29 16:06:25 +0300

    Second Commit

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -1,9 +1,15 @@
 package org.kodluyoruz;
 import java.util.*;
 public class Main {
-
-
 	
+	public static void printMatrisAndPoints(char[][]matris) {
+		for(char[]row:matris) {
+			for(char cell:row) {
+				System.out.print(cell+"|");
+			}
+			System.out.println("");
+    	}
+	}
 	
 	public static boolean isAllCellsFull(char[][]matris) {
 		int flag=0;
@@ -29,7 +35,7 @@ public class Main {
     	Scanner input=new Scanner(System.in);
     	Random rand = new Random();
     	int n,player; //n:matris boyutu,player oyunu oynayacak kiþi(0=>bilgisayar,1=>kullanýcý)
-    	
+    	char letterPc=' ',letterPlayer=' ';
 		
 		  do { 
 			  System.out.print("Size of matrix(3...7):");
@@ -42,11 +48,39 @@ public class Main {
     	
     	player=rand.nextInt(2);
     	if(player==0) {
-    		System.out.println("Bilgisayar");
+    		System.out.println("Bilgisayar basliyor.");
+    		int letter=rand.nextInt(2);
+    		if(letter==0) {
+    			letterPc='S';
+    			letterPlayer='O';
+    		}
+    		else {
+    			letterPc='O';
+    			letterPlayer='S';
+    		}
+    		System.out.println("Bilgisayarýn sectigi harf:"+letterPc);
+    		System.out.println("Oyuncuya kalan harf:"+letterPlayer);
     	}
+    	
     	else {
-    		System.out.println("Oyuncu");
+    		System.out.println("Oyuncu basliyor"); 	
+    		System.out.print("Harf girin:");
+			letterPlayer= input.next().charAt(0);
+    		/*do {
+    			System.out.print("Harf girin:");
+    			letterPlayer= input.next().charAt(0);
+    		}
+    		while(!(letterPlayer=='O' || letterPlayer=='S'));*/
+    		System.out.println("Oyuncunun sectigi harf:"+letterPlayer);
+    		if(letterPlayer=='S') {
+    			letterPc='O';
+    		}
+    		else {
+    			letterPc='S';
+    		}
+    		System.out.println("Bilgisayara kalan harf:"+letterPc);
     	}
+    	
     	
     	for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
@@ -54,14 +88,15 @@ public class Main {
 			}
     	}
     	
-    	for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				System.out.print(matris[i][j]+"|");
-			}
-			System.out.println("");
+    	while(!isAllCellsFull(matris)) {
+    		
     	}
     	
+    	printMatrisAndPoints(matris);
     	System.out.println(isAllCellsFull(matris));
+    
     	
+    	
+    	input.close();
     }
 }
