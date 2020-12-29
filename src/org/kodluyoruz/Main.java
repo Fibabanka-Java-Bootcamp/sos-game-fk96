f33ebb24eded1d299ad71326c57bce4705cbfa27
fk96 <furkankale.1996@hotmail.com>
2020-12-29 23:56:41 +0300

    final commit

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -11,6 +11,16 @@ public class Main {
     	}
 	}
 	
+	public static boolean isCellEmpty(char[][]matris,int i,int j) {
+		if(matris[i][j]=='S' || matris[i][j]=='O') {
+			return false; 
+		}
+		else {
+			return true;
+		}
+	}
+	
+	
 	public static boolean isAllCellsFull(char[][]matris) {
 		int flag=0;
 		for(char[]row:matris) {
@@ -34,7 +44,7 @@ public class Main {
     public static void main(String[] args) {
     	Scanner input=new Scanner(System.in);
     	Random rand = new Random();
-    	int n,player; //n:matris boyutu,player oyunu oynayacak kiþi(0=>bilgisayar,1=>kullanýcý)
+    	int i,j,n,player; //n:matris boyutu,player oyunu oynayacak kiþi(0=>bilgisayar,1=>kullanýcý)
     	char letterPc=' ',letterPlayer=' ';
 		
 		  do { 
@@ -43,8 +53,13 @@ public class Main {
 		  while(n<3 || n>7);
 		  
 		  //System.out.println(n); 
-		  char matris[][]=new char[n][n];
+		char matris[][]=new char[n][n];
 		 
+		for(i=0;i<n;i++) {
+			for(j=0;j<n;j++) {
+				matris[i][j]='_';
+			}
+    	}
     	
     	player=rand.nextInt(2);
     	if(player==0) {
@@ -58,8 +73,30 @@ public class Main {
     			letterPc='O';
     			letterPlayer='S';
     		}
-    		System.out.println("Bilgisayarýn sectigi harf:"+letterPc);
-    		System.out.println("Oyuncuya kalan harf:"+letterPlayer);
+    		while(!isAllCellsFull(matris)) {
+    			i=rand.nextInt(n);
+        		j=rand.nextInt(n);
+        		
+        		if(isCellEmpty(matris,i,j)) {
+        			matris[i][j]=letterPc;
+        		}
+        		//puan hesaplama,matris yazdirma ve modülü yazilacak
+        		System.out.print("Satir ve sutun bilgisini giriniz");
+        		do {
+        			i=input.nextInt();
+            		j=input.nextInt();
+        		}
+        		while((i<0 && i>n-1) && (j<0 && j>n-1));
+        		if(isCellEmpty(matris,i,j)) {
+        			matris[i][j]=letterPlayer;
+        		}
+        		//puan hesaplama,matris yazdirma ve modülü yazilacak
+    		}
+    		
+    		
+    		
+    		//System.out.println("Bilgisayarýn sectigi harf:"+letterPc);
+    		//System.out.println("Oyuncuya kalan harf:"+letterPlayer);
     	}
     	
     	else {
@@ -71,31 +108,38 @@ public class Main {
     			letterPlayer= input.next().charAt(0);
     		}
     		while(!(letterPlayer=='O' || letterPlayer=='S'));*/
-    		System.out.println("Oyuncunun sectigi harf:"+letterPlayer);
+			//System.out.println("Oyuncunun sectigi harf:"+letterPlayer);
     		if(letterPlayer=='S') {
     			letterPc='O';
     		}
     		else {
     			letterPc='S';
     		}
-    		System.out.println("Bilgisayara kalan harf:"+letterPc);
-    	}
-    	
-    	
-    	for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				matris[i][j]='_';
-			}
-    	}
-    	
-    	while(!isAllCellsFull(matris)) {
+    		//System.out.println("Bilgisayara kalan harf:"+letterPc);
+    		while(!isAllCellsFull(matris)) {
+    			
     		
+    		System.out.print("Satir ve sutun bilgisini giriniz");
+    		do {
+    			i=input.nextInt();
+        		j=input.nextInt();
+    		}
+    		while((i<0 && i>n-1) && (j<0 && j>n-1));
+    		//puan hesaplama,matris yazdirma ve modülü yazilacak
+    		i=rand.nextInt(n);
+    		j=rand.nextInt(n);
+    		
+    		if(isCellEmpty(matris,i,j)) {
+    			matris[i][j]=letterPc;
+    		}
+    		
+    		//puan hesaplama,matris yazdirma ve modülü yazilacak
+    		}
     	}
+    	System.out.println("PC:"+letterPc+" Player:"+letterPlayer);
     	
-    	printMatrisAndPoints(matris);
-    	System.out.println(isAllCellsFull(matris));
-    
     	
+       	
     	
     	input.close();
     }
